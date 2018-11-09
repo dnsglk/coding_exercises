323760486472218d4bea2bbc2549020c2d2c32e5
Denys Gulak <dgulak@luxoft.com>
2018-11-09 13:14:42 +0100

    Continue work on 4.4

--- crack-code-inter/4.4.py
+++ crack-code-inter/4.4.py
@@ -3,12 +3,14 @@ import random
 from binarytree import tree 
 
 '''
+DOESN'T WORK!!!
+
 Balanced tree should have difference between max height
 and min heigt no more than 1. Max height is the max distance from
 root to leaf. Min height is the min distance from the root to the
 first null child in any node.
 '''
-def isBalanced(tree):
+def _isBalanced(tree):
     
     hMax = getMaxHeight(tree)
     hMin = getMinHeight(tree)
@@ -26,17 +28,6 @@ def getMinHeight(node):
 
     return hL if hL < hR else hR
 
-def isBalanced2(tree):    
-    if tree.left != None and isBalanced(tree.left) == False:
-        return False
-    if tree.right != None and isBalanced(tree.right) == False:
-        return False
-
-    hL = getHeight(tree.left)
-    hR = getHeight(tree.right)
-    
-    return True if (hL - hR) ^ 2 <= 1 else False
-
 def getMaxHeight(node):
     if node == None:
         return 0
@@ -49,17 +40,58 @@ def getMaxHeight(node):
 
     return hL + 1 if hL > hR else hR + 1
 
+
+
+'''
+Recursively check that each subtree doesn't have difference in 
+height more than by 1 point.
+'''
+def isBalanced(node):    
+    if node == None or (node.left == None and node.right == None):
+        return True
+
+    results_left = getHeight(node.left)
+    results_right = getHeight(node.right)
+    hL, hR = results_left['height'], results_right['height']
+    left_is_balanced, right_is_balanced = results_left['is_balanced'], results_right['is_balanced']
+ 
+    return True if left_is_balanced and right_is_balanced and abs(hL - hR) <= 1 else False
+
+def getHeight(node):
+    if node == None:
+        return {"is_balanced": True, "height": 0}
+
+    results = getHeight(node.left) 
+    hL, is_balanced = results['height'] + 1, results['is_balanced']
+    if not is_balanced:
+        print "Not balanced ", hL, node.value
+        return {'height':hL, 'is_balanced': False} 
+
+    results_tupple = getHeight(node.right)
+    hR, is_balanced = results['height'] + 1, results['is_balanced']
+    if not is_balanced:
+        print "Not balanced ", hR, node.value
+        return {'height':hR, 'is_balanced' : False}
+
+    maxH = hL if hL > hR else hR
+    balanced =  abs(hL - hR) <= 1
+
+    print node.value, hL, hR, balanced
+
+    return {'height': maxH, 'is_balanced': balanced}
+    
+
 def run_algo(iter_num):
     for i in range(iter_num):
-        test_tree =  tree(height=random.randint(0,6), is_perfect=False)
+        test_tree =  tree(height=random.randint(0,4), is_perfect=False)
         testTreeIsBalanced = isBalanced(test_tree) 
 
         if testTreeIsBalanced != test_tree.is_balanced:
             print(test_tree)
-            print 'Tree is ', '' if test_tree.is_balanced else 'not', ' balanced. Manual algo returns ', testTreeIsBalanced
+            print 'Tree is', '' if test_tree.is_balanced else ' not', ' balanced. Manual algo returns ', testTreeIsBalanced
             print 'Max,min = ', getMaxHeight(test_tree), getMinHeight(test_tree)
-            return
+            #return
     
 
 if __name__  == "__main__":
-    run_algo(1000)
+    run_algo(100)
